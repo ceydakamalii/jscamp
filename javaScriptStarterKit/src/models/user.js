480753a750c98229f48e83a210914f60bc2e1d2a
Ceyda Kamalı <ceydakamali3@gmail.com>
2021-11-12 18:47:11 +0300

    08.11.2021

---
+++ javaScriptStarterKit/src/models/user.js
@@ -0,0 +1,8 @@
+export default class User {
+    constructor(id, fistName, lastName, city) {
+        this.id = id,
+        this.fistName = fistName,
+        this.lastName = lastName,
+        this.city = city
+    }
+}
